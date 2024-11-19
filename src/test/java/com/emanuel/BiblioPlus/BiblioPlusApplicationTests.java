c813a9a0b92be625e37966b7714f6d3b0589ff6b
EmanuelErnesto <emanuel.earego@gmail.com>
2024-11-19 15:35:12 -0300

    test: add default test file

---
+++ src/test/java/com/emanuel/BiblioPlus/BiblioPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel.BiblioPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
