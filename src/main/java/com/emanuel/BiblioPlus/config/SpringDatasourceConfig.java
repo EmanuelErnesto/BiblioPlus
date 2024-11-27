adec243f080c276590c4fa4ac62ada01e048aa16
EmanuelErnesto <emanuel.earego@gmail.com>
2024-11-27 01:28:35 -0300

    raw: add profile to datasource config

--- src/main/java/com/emanuel/BiblioPlus/config/SpringDatasourceConfig.java
+++ src/main/java/com/emanuel/BiblioPlus/config/SpringDatasourceConfig.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("default")
 public class SpringDatasourceConfig {
 
     @Autowired
